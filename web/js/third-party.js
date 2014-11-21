88f80ef917dae42e93094c732503347df776250c
Brian Steere <dianoga7@3dgo.net>
2014-11-21 15:27:55 -0600

    Add lots of files locally instead of pulling from cdns. Most things are condensed into a single file

---
+++ web/js/third-party.js
@@ -0,0 +1,7 @@
+//@prepros-append ../third-party/backbone.marionette/jquery.js
+//@prepros-append ../third-party/backbone.marionette/underscore.js
+//@prepros-append ../third-party/backbone.marionette/backbone.js
+//@prepros-append ../third-party/backbone.marionette/backbone.marionette.js
+//@prepros-append ../third-party/backbone.stickit/backbone.stickit.js
+//@prepros-append ../third-party/skycons/skycons.js
+//@prepros-append ../third-party/jBox/jBox.js
